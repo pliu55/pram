853702d5383175d1ef62e05f9e3a3345e1d2bba6
Peng Liu <pliu55@wisc.edu>
2017-05-21 17:35:30 -0500

    simplify PRAM installation script

---
+++ job.R
@@ -0,0 +1,13 @@
+#!/bin/env Rscript
+
+library(devtools)
+library(roxygen2)
+library(testthat)
+
+setwd('/ua/pliu/repe/pram/')
+#document()
+install(quick=T, reload=F, threads=4)
+
+
+#test( filter = 'ModelGTF' )
+#test( )
