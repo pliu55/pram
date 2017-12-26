d78a043f1c40b52cd44f6990d5d779a957c954ac
Peng Liu <pliu55@wisc.edu>
2017-12-25 20:33:00 -0600

    update importFrom FilterRules

--- R/prepIgBam.R
+++ R/prepIgBam.R
@@ -49,7 +49,7 @@ defManager <- function(fuserbams, outdir, prm) {
 
 #' @importFrom  data.table  setkey data.table
 #' @importFrom  Rsamtools   indexBam filterBam BamFile ScanBamParam
-#' @importFrom  S4Vectors   filterRules
+#' @importFrom  S4Vectors   FilterRules
 #'
 prepIgBamByFile <- function(finbam, iggrs, managerdt, prm) {
     foutbam = managerdt[ fuserbam == finbam ]$foutbam
