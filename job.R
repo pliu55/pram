c9e853c0f61ae6325f417aa4edec5dd28f415d85
Peng Liu <pliu55@wisc.edu>
2018-04-26 08:36:44 -0500

    ignore installation script

--- job.R
+++ job.R
@@ -8,6 +8,8 @@ setwd('/ua/pliu/repe/pram/')
 document()
 install(quick=T, reload=F, threads=4)
 
+## deal with prepIgBam's selAlnInGRanges: readGAlignment, read in chunk
+## debug by ~/repe/gata/70_prepIgBam/
 
 # test( filter = 'screenModel' )
 # test( filter = 'prepIgBam' )
