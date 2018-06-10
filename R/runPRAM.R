c3fa68db36f9a0a929b58e2c869c5637dde3841f
Peng Liu <pliu55@wisc.edu>
2018-06-10 08:01:35 -0500

    update example in doc

--- R/runPRAM.R
+++ R/runPRAM.R
@@ -1,4 +1,4 @@
-#' Predict intergenic transcript models from RNA-seq and screen them by ChIP-seq
+#' @title Predict intergenic transcript models from RNA-seq and screen them by ChIP-seq
 #'
 #' @inheritParams defIgRanges
 ##                to inherit `in_gtf`
@@ -14,6 +14,34 @@
 #' @importFrom  tools  file_path_sans_ext
 #'
 #' @export
+#'
+#' @examples
+#'
+#' in_gtf = system.file('extdata/demo/in.gtf', package='pram')
+#'
+#' in_bamv = c( system.file('extdata/demo/SZP.bam', package='pram'),
+#'              system.file('extdata/demo/TLC.bam', package='pram') )
+#'
+#' pred_out_gtf = tempfile(fileext='.gtf')
+#'
+#' runPRAM(in_gtf, in_bamv, pred_out_gtf)
+#'
+#'
+#' in_bedv = c( system.file('extdata/demo/H3K79me2.bed.gz', package='pram'),
+#'              system.file('extdata/demo/POLR2.bed.gz',    package='pram') )
+#'
+#' training_tpms = c( system.file('extdata/demo/AED1.isoforms.results',
+#'                                package='pram'),
+#'                    system.file('extdata/demo/AED2.isoforms.results',
+#'                                package='pram') )
+#'
+#' training_gtf = system.file('extdata/demo/training.gtf', package='pram')
+#'
+#' screen_out_gtf = tempfile(fileext='.gtf')
+#'
+#' runPRAM(in_gtf, in_bamv, screen_out_gtf, in_bedv, training_tpms,
+#'         training_gtf)
+#'
 #'
 runPRAM <- function(in_gtf, in_bamv, out_gtf, in_bedv, training_tpms,
                     training_gtf) {
