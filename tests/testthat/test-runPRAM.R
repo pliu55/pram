e98067146b12dccd825abf601c0e47d8e7fb6cfb
Peng Liu <pliu55@wisc.edu>
2018-09-12 08:38:26 -0500

    comment out screenModel-related functions/text

--- tests/testthat/test-runPRAM.R
+++ tests/testthat/test-runPRAM.R
@@ -11,20 +11,20 @@ main <- function() {
     in_bamv = c( system.file('extdata/demo/SZP.bam', package='pram'),
                  system.file('extdata/demo/TLC.bam', package='pram') )
 
-    in_bedv = c( system.file('extdata/demo/H3K79me2.bed.gz', package='pram'),
-                 system.file('extdata/demo/POLR2.bed.gz',    package='pram') )
+  # in_bedv = c( system.file('extdata/demo/H3K79me2.bed.gz', package='pram'),
+  #              system.file('extdata/demo/POLR2.bed.gz',    package='pram') )
 
-    training_tpms = c( system.file('extdata/demo/AED1.isoforms.results',
-                                   package='pram'),
-                       system.file('extdata/demo/AED2.isoforms.results',
-                                   package='pram') )
+  # training_tpms = c( system.file('extdata/demo/AED1.isoforms.results',
+  #                                package='pram'),
+  #                    system.file('extdata/demo/AED2.isoforms.results',
+  #                                package='pram') )
 
-    training_gtf = system.file('extdata/demo/training.gtf', package='pram')
+  # training_gtf = system.file('extdata/demo/training.gtf', package='pram')
 
     testPred(in_gtf, in_bamv, pred_out_gtf)
 
-    testPredScreen(in_gtf, in_bamv, screen_out_gtf, in_bedv, training_tpms,
-                   training_gtf)
+  # testPredScreen(in_gtf, in_bamv, screen_out_gtf, in_bedv, training_tpms,
+  #                training_gtf)
 }
 
 
@@ -35,12 +35,12 @@ testPred <- function(in_gtf, in_bamv, out_gtf) {
 }
 
 
-testPredScreen <- function(in_gtf, in_bamv, out_gtf, in_bedv, training_tpms,
-                           training_gtf) {
-    runPRAM(in_gtf, in_bamv, out_gtf, in_bedv, training_tpms, training_gtf)
-    test_that('runPRAM::testPredScreen',
-              expect_true( file.exists(out_gtf)))
-}
+# testPredScreen <- function(in_gtf, in_bamv, out_gtf, in_bedv, training_tpms,
+#                          training_gtf) {
+#   runPRAM(in_gtf, in_bamv, out_gtf, in_bedv, training_tpms, training_gtf)
+#   test_that('runPRAM::testPredScreen',
+#             expect_true( file.exists(out_gtf)))
+# }
 
 
 main()
--- tests/testthat/test-screenModel.R
+++ tests/testthat/test-screenModel.R
@@ -1,33 +1,33 @@
 library(data.table)
 
 main <- function() {
-    context('screenModel')
+#   context('screenModel')
 
-    fbeds = c( system.file('extdata/bed/GM12878POLR2AphosphoS5Rep1.bed',
-                           package='pram'),
-               system.file('extdata/bed/GM12878POLR2AphosphoS5Rep2.bed',
-                           package='pram') )
+#   fbeds = c( system.file('extdata/bed/GM12878POLR2AphosphoS5Rep1.bed',
+#                          package='pram'),
+#              system.file('extdata/bed/GM12878POLR2AphosphoS5Rep2.bed',
+#                          package='pram') )
 
-    ftpms = c( system.file('extdata/rsem/GM12878Rep1_training.isoforms.results',
-                           package='pram'),
-               system.file('extdata/rsem/GM12878Rep2_training.isoforms.results',
-                           package='pram') )
+#   ftpms = c( system.file('extdata/rsem/GM12878Rep1_training.isoforms.results',
+#                          package='pram'),
+#              system.file('extdata/rsem/GM12878Rep2_training.isoforms.results',
+#                          package='pram') )
 
-    fgtf_training = system.file('extdata/gtf/rf_training.gtf', package='pram')
-    fgtf_testing  = system.file('extdata/gtf/rf_testing.gtf',  package='pram')
+#   fgtf_training = system.file('extdata/gtf/rf_training.gtf', package='pram')
+#   fgtf_testing  = system.file('extdata/gtf/rf_testing.gtf',  package='pram')
 
-    tmpdir = '/tier2/deweylab/scratch/pliu/repe/pram/'
-    fgtf_out = paste0(tmpdir, 'rf.gtf')
-    if ( file.exists(fgtf_out) ) file.remove(fgtf_out)
+#   tmpdir = '/tier2/deweylab/scratch/pliu/repe/pram/'
+#   fgtf_out = paste0(tmpdir, 'rf.gtf')
+#   if ( file.exists(fgtf_out) ) file.remove(fgtf_out)
 
-   #outl = trainModelClassifier(fbeds, ftpms, fgtf_training, outdir,
-   #                            expr_min_tpm=1, cv_n_folds=10, nthreads=1)
+#  #outl = trainModelClassifier(fbeds, ftpms, fgtf_training, outdir,
+#  #                            expr_min_tpm=1, cv_n_folds=10, nthreads=1)
 
-    screenModel(fbeds, ftpms, fgtf_training, fgtf_testing, fgtf_out, tmpdir,
-                expr_min_tpm=1, cv_n_folds=10, nthreads=1)
+#   screenModel(fbeds, ftpms, fgtf_training, fgtf_testing, fgtf_out, tmpdir,
+#               expr_min_tpm=1, cv_n_folds=10, nthreads=1)
 
-    test_that(paste0('screenModel:', fgtf_out),
-              expect_true( file.exists(fgtf_out) ) )
+#   test_that(paste0('screenModel:', fgtf_out),
+#             expect_true( file.exists(fgtf_out) ) )
 }
 
 main()
