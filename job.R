fa4427386671e9d868f24fa15fac4eea65b55b8f
Peng Liu <pliu55@wisc.edu>
2018-09-12 09:00:22 -0500

    ignore installation script

--- job.R
+++ job.R
@@ -1,15 +1,16 @@
 #!/usr/bin/env Rscript
 
-library(devtools)
-library(roxygen2)
-library(testthat)
+suppressMessages(library(devtools))
+suppressMessages(library(roxygen2))
+suppressMessages(library(testthat))
 # library(BiocStyle)
 
 setwd('/ua/pliu/repe/pram/')
+
 # build_vignettes()
 # document()
-  install(quick=T, reload=T, build_vignettes=T, threads=4)
-# install(quick=F, reload=F, build_vignettes=F, threads=4)
+# install(quick=T, reload=T, build_vignettes=T, threads=4)
+
 
 ## deal with prepIgBam's selAlnInGRanges: readGAlignment, read in chunk
 ## debug by ~/repe/gata/70_prepIgBam/
@@ -22,4 +23,6 @@ setwd('/ua/pliu/repe/pram/')
 # test( filter = 'selModel' )
 # test( filter = 'GTF' )
 # test( filter = 'runPRAM' )
+
+  install(quick=F, reload=F, build_vignettes=F, threads=4)
   test()
