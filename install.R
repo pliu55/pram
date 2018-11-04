5eb2d3d56eb1b35b3889089e17172f24ca743bd0
Peng Liu <pliu55@wisc.edu>
2018-11-04 08:58:11 -0600

    update

--- install.R
+++ install.R
@@ -4,6 +4,12 @@ library(devtools)
 library(roxygen2)
 library(testthat)
 
-document()
-install(quick=F, reload=T, build_vignettes=T, threads=4)
-test()
+setwd('/ua/pliu/repe/pram/')
+
+# document()
+# install(quick=F, reload=T, build_vignettes=T, threads=4)
+# test()
+
+build_vignettes()
+
+# test( filter = 'runPRAM' )
