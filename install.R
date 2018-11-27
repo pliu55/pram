0c6f7ec896e1f477df9dcc8da4afd5f4b6beec7c
Peng Liu <pliu55@wisc.edu>
2018-11-27 09:33:58 -0600

    check package

--- install.R
+++ install.R
@@ -7,8 +7,9 @@ library(testthat)
 setwd('/ua/pliu/repe/pram/')
 
   document()
-  install(quick=F, reload=T, build_vignettes=T, threads=4)
-  test()
+# install(quick=F, reload=T, build_vignettes=T, threads=4)
+# test()
+  check(cran=F)
 
 # build_vignettes()
 
